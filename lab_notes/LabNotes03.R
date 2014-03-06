1254b423ff1e95c67b51f878bd8bb7d5bead769f
Aaron Hill <aaron.hill@me.com>
2014-03-06 08:17:54 -0500

    in class exercise completed

--- lab_notes/LabNotes03.R
+++ lab_notes/LabNotes03.R
@@ -10,12 +10,15 @@ eval(parse(text=xadmin), envir= .GlobalEnv)
 
 ### RECODE THE VARIABLE physicians INTO AN ORDINAL VARIABLE
 
+physicians.description
+
 # First, look at the distribution of physicians
 summary(country$physicians)
 hist(country$physicians)
 quantile(country$physicians, seq(0, 1, 0.1), na.rm=TRUE)
 
 # Next, use subset assignment statements to assign an ordinal coding scheme for a new variable: physiciansCat
+
 country$physiciansCat[country$physicians <= 25] <- 1
 country$physiciansCat[country$physicians > 25 & country$physicians <= 100] <- 2
 country$physiciansCat[country$physicians > 100 & country$physicians <= 250] <- 3
@@ -43,3 +46,29 @@ tapply(country$physicians, country$physiciansCat, summary)
 # 4. Create the description variable
 # 5. Check your work
 # 6. Create a crosstab of physicians and infant mortality
+
+mort2004.description
+
+# First, look at the distribution of physicians
+summary(country$mort2004)
+hist(country$mort2004)
+quantile(country$mort2004, seq(0, 1, 0.05), na.rm=TRUE)
+
+# Next, use subset assignment statements to assign an ordinal coding scheme for a new variable: physiciansCat
+
+country$mort2004Cat[country$mort2004 <= 15] <- 1
+country$mort2004Cat[country$mort2004 > 15 & country$mort2004 <= 50] <- 2
+country$mort2004Cat[country$mort2004 > 50] <- 3
+
+# Convert the new variable to a "factor" (to indicate that it's nominal or ordinal) and assign value labels
+country$mort2004Cat <- factor(country$mort2004Cat, labels=c('Low: 0-15 infant deaths per 1k', 'Medium: 16-50 infant deaths per 1k', 'High: >50 infant deaths per 1k'))
+
+# Create a description variable to document the nature of your new variable
+country$mort2004Cat.description <- "ORDINAL: Infant mortality rate"
+
+# Check your work: 
+# 1. look at the distribution of the new variable and compare it to the distribution of the old variable
+# 2. use the tapply funtion to summarize the old variable across categories of the new ordinal variable
+table(country$mort2004Cat, useNA="ifany") # distribution of new variable (including missing values)
+summary(country$mort2004) # distribution of old variable
+tapply(country$mort2004, country$mort2004Cat, summary)
