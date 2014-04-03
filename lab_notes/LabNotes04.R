97313b8a9726bdd26c1719176f99aa8c58511d82
Aaron Hill <aaron.hill@me.com>
2014-04-03 16:46:02 -0400

    for anova lecture

---
+++ lab_notes/LabNotes04.R
@@ -0,0 +1,35 @@
+###################################################################################################
+################# ADMIN CODE -- DO NOT MODIFY #####################################################
+options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+library(RCurl)
+xadmin <- getURL("https://raw.github.com/aaronxhill/quant14sp/master/admin/admin.R")
+eval(parse(text=xadmin), envir= .GlobalEnv)
+###################################################################################################
+
+### dataset: states
+
+# Two-Sample Hypothesis Test
+
+### Level of measurement:# The dependent variable must be interval-ratio.# The independent variable must be nominal or ordinal, with two categories to be tested (this variable can have more than two categories, but with this test you will only be testing for a difference between only two of them).  
+
+# Compare the variable turnout (% of voting age population that voted) by Regions Northeast and West
+# Dependent variable is turnout and Independent variable is region.  This test will determine if there is a difference in average voter turnout between states in the Northeast and states in the West.  
+
+### GENERAL SYNTAX
+# t.test(y~x, dataset)
+
+### SYNTAX FOR THIS EXAMPLE
+t.test(TURNOUT~REGION, states) # BUT, there is a problem! REGION has four groups, and t-tests are for two groups
+
+# when testing the difference between two groups when there are more than two, subset the data to include only the two groups that are going to be tested:
+states.subset <- states[states$REGION %in% c("Northeast", "West"), ]
+
+# look at the means differences between the groups:
+aggregate(TURNOUT~REGION, states.subset, mean)
+
+# run the test: 
+t.test(TURNOUT~REGION, states.subset)
+
+# interpreting the results: 
+
+# First, evaluate the t score. Here, it is 2.266. Next, is it significant? The p-value is 0.03499. Since this is less than .05 it is significant.# Technically reporting Independent Sample T-Tests:# There was a significant effect for region, t(19.517) = 2.266, p = .035 with Northeast having a higher turnout than West.
---
+++ lab_notes/LabNotes05.R
@@ -0,0 +1,41 @@
+###################################################################################################
+################# ADMIN CODE -- DO NOT MODIFY #####################################################
+options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+library(RCurl)
+xadmin <- getURL("https://raw.github.com/aaronxhill/quant14sp/master/admin/admin.R")
+eval(parse(text=xadmin), envir= .GlobalEnv)
+###################################################################################################
+
+### dataset: states
+
+# ANOVA
+
+### Level of measurement:# The dependent variable must be interval-ratio.# The independent variable must be nominal or ordinal, with more than two categories to be tested.
+
+# Test whether the poverty rate differs across regions of the U.S.  # Dependent variable: poverty# Independent variable: region
+
+### GENERAL SYNTAX
+# aggregate(y~x, dataset, mean) # look at the means by category
+# anova.results <- aov(y~x, dataset) # use the aov function to compute anova and store the results in the variable anova.results
+# summary(anova.results) # show the table of results
+
+
+### SYNTAX FOR THIS EXAMPLE
+
+# look at the means by REGION
+aggregate(POVERTY~REGION, states, mean) 
+
+# use the aov function to compute anova and store the results in the variable anova.results
+anova.results <- aov(POVERTY~REGION, states) 
+
+# show the table of results
+summary(anova.results) 
+
+# interpreting the results: 
+
+# Does F-obtained (F value) fall in the critical region?  In other words, is the difference statistically significant? If the value of Pr(>F) is smaller than alpha (typically 0.05), we will reject the null hypothesis that there is no difference among the three populations represented by the three groups.  If the value of Pr(>F) is greater than 0.05, we will fail to reject the null hypothesis.# Technically reporting ANOVA:# We examined poverty by region.  There was a significant main effect of region F(3,46) = 8.284, p < 0.001. The highest level of poverty is in the South, and the lowest is in the Northeast. 
+
+### ADVANCED: 
+
+# Multiple comparisons: 
+TukeyHSD(anova.results)
